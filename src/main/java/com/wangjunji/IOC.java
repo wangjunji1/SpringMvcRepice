1946a4dc44fa9184e02134f6b848e2f9a878d89c
wangjunji <344783488@qq.com>
2019-09-12 09:31:50 +0800

    第一次提交基础信息

--- src/main/java/com/wangjunji/IOC.java
+++ src/main/java/com/wangjunji/IOC.java
@@ -1,8 +1,9 @@
 package com.wangjunji;
 
 public class IOC {
-    @Override
+
     public String toString() {
-        return super.toString();
+        System.out.println("111111");
+        return null;
     }
 }
